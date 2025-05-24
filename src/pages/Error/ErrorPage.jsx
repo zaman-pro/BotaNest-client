d8cdd92e9f3fbd4abe26d745d657b94d4d27b795
unknown <zaman-pro>
2025-05-25 05:28:59 +0600

    lottie react implement on the error page

--- src/pages/Error/ErrorPage.jsx
+++ src/pages/Error/ErrorPage.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 import { Link } from "react-router";
+import Lottie from "lottie-react";
 import useTitle from "../../utils/useTitle";
+import errorAnimation from "../../assets/Animation - 1748126991659.json";
 
 const ErrorPage = () => {
   useTitle("Error - BotaNest");
+
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <div className="flex flex-col justify-center items-center gap-8 min-h-screen">
-        <h1 className="text-9xl font-bold text-gray-400">404</h1>
-        <h2 className="text-xl font-medium md:text-4xl">
-          Sorry, we couldn't find this page.
-        </h2>
-        <Link to="/" className="btn btn-error">
-          Back to Home
-        </Link>
+      <div className="w-full max-w-[500px] h-[300px] sm:h-[400px] md:h-[500px]">
+        <Lottie animationData={errorAnimation} loop={true} />
       </div>
     </div>
   );
